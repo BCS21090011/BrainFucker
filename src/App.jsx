93075195976420d6fbdb3ab8c06f179f6b486f72
BCS21090011 <100390618+BCS21090011@users.noreply.github.com>
2025-05-29 23:29:51 +0800

    Still trying to create terminal

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,64 @@ import "./App_My.css";
 import BrainfuckExecuter from "./Executer/BrainfuckExecuter";
 
 function App() {
+  const bfCodeTextareaRef = useRef(null);
   const terminalRef = useRef(null);
   const outputPromptRef = useRef(null);
   const inputPromptRef = useRef(null);
 
   const [outputPromptString, setOutputPromptString] = useState("");
 
+  function OutputToOutputPrompt (text) {
+    setOutputPromptString(outputPromptString + text);
+  }
+
   function InputPrompt_OnKeyDown (e) {
     if (e.key === "Enter") {
-      let inputString = e.target.textContent + '\n';
-      e.target.textContent = ""; // Clear the input prompt.
+      let inputString = e.target.value;
+      inputString += '\n';
+      console.log(inputString);
 
-      setOutputPromptString(inputString);
+      OutputToOutputPrompt(inputString);
+      e.target.value = ""; // Clear the input prompt.
     }
+  }
+
+  const bfObj = new BrainfuckExecuter();
+  bfObj.OutputCallback = (output, brainfuckExecuter) => {
+    OutputToOutputPrompt(String.fromCharCode(output));
   }
 
   return (
     <>
-      <div
-        ref={terminalRef}
-        className="terminal"
-        onClick={() => inputPromptRef.current?.focus()}
+      <textarea
+        id="BFCodeTextarea"
+        ref={bfCodeTextareaRef}
+        placeholder="Enter your Brainfuck code here..."
       >
-        <pre
-          ref={outputPromptRef}
-        >
+      </textarea>
+      <button onClick={() => {
+        bfObj.BFCode = bfCodeTextareaRef.current.value;
+      }}>Apply BF code</button>
+      <button onClick={() => {
+        while (bfObj.CodeEnded == false) {
+          bfObj.BF_Execute();
+        }
+      }}>
+        Execute until end
+      </button>
+      <button onClick={() => { OutputToOutputPrompt(prompt("Your output:")); }}>Output things</button>
+      <div
+        id="TerminalDiv"
+        ref={terminalRef}
+      >
+        <span id="OutputPromptSpan" ref={outputPromptRef}>
           {outputPromptString}
-        </pre>
-        <pre
+        </span>
+        <input
+          id="InputPromptInput"
           ref={inputPromptRef}
-          contentEditable={true}
           onKeyDown={InputPrompt_OnKeyDown}
-        >
-        </pre>
+        />
       </div>
     </>
   )
